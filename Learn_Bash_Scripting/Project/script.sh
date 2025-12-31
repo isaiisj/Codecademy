9a13ee1da3f4478e4e75edcee6ce655e67388108
Isai <47548422+isaiisj@users.noreply.github.com>
2025-12-31 02:11:36 -0600

    Update script.sh

--- Learn_Bash_Scripting/Project/script.sh
+++ Learn_Bash_Scripting/Project/script.sh
@@ -1,1 +1,39 @@
+#!/bin/bash
+echo "Welcome to my bash script :D"
 
+firstline=$(head -n 1 source/changelog.md)
+
+read -a splitfirstline <<< $firstline
+
+version=${splitfirstline[1]}
+echo "You are building version " $version
+
+echo "Do you want to continue? [y(1)/n(0)]"
+read versioncontinue
+
+if [ $versioncontinue -eq 1 ]
+then 
+  echo "OK"
+else
+  echo "Please come back when you are ready"
+fi
+
+
+for filename in source/*
+do
+  echo $filename
+  if [ $filename == "source/secretinfo.md" ]
+  then
+    echo "Not copying" $filename
+  else
+    echo "Copying" $filename
+    cp $filename build/.
+  fi
+done
+
+cd build/
+# We'll add more code here later
+cd ..
+
+echo "Build version $version contains: "
+ls
